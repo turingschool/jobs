bcbaeb4b4be2b1b82b7e30dd5881e77dfa25b510
awortham <wortham.aaron@gmail.com>
2015-01-14 09:51:22 -0700

    changed root and added home controller, home view

--- test/features/paths_spec.rb
+++ test/features/paths_spec.rb
@@ -1,7 +1,7 @@
 require './test/test_helper'
 
 class Paths < ActionDispatch::IntegrationTest
-  def can_visit_the_root_path_without_authentication
+  def test_can_visit_the_root_path_without_authentication
     visit '/'
     assert page_has_content? "Login with GitHub"
   end
