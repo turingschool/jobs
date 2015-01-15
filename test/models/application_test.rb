279748cf3dd0c5fa6330f2995b1aa4eb6b8b922c
Glen Egbert <glenegbert1@gmail.com>
2015-01-15 09:53:42 -0700

    added additional applications model test

--- test/models/application_test.rb
+++ test/models/application_test.rb
@@ -10,6 +10,13 @@ class ApplicationTest < ActiveSupport::TestCase
   def test_it_returns_applications_with_different_statuses
     application1 = create(:application)
     application2 = create(:application, status: "in-progress")
+    application3 = create(:application, status: "applied")
+    application4 = create(:application, status: "closed")
+
+    assert_equal application1, Application.to_apply.first
+    assert_equal application2, Application.in_progress.first
+    assert_equal application3, Application.applied.first
+    assert_equal application4, Application.closed.first
   end
 
 end
