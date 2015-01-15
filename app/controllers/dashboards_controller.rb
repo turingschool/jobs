ef5c6cccd93df810a906da5d8db5b5382b9a8844
awortham <wortham.aaron@gmail.com>
2015-01-15 09:14:56 -0700

    changed filters in controller and deleted out status from view because we filter by status

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,8 +1,14 @@
 class DashboardsController < ApplicationController
   def show
     unless current_person.kind_of? String
-      @applications = current_person.applications.active
-      @dead_applications = current_person.applications.dead
+      @to_apply_applications = application_search(to_apply)
+      @closed_applications = application_search(closed)
+      @in_progress_applications = application_search(in_progress)
+      @applied_applications = application_search(applied)
     end
+  end
+
+  def application_search(type)
+    current_person.applications.status == type
   end
 end
