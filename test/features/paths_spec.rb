d9955c9116ed28f133dfd37d8d0c1f573404b89c
awortham <wortham.aaron@gmail.com>
2015-01-15 08:56:00 -0700

    minor changes

--- test/features/paths_spec.rb
+++ test/features/paths_spec.rb
@@ -3,6 +3,6 @@ require "./test/test_helper"
 class Paths < ActionDispatch::IntegrationTest
   def test_can_visit_the_root_path_without_authentication
     visit "/"
-    assert page_has_content? "Login with GitHub"
+    assert page.has_content? "Login with GitHub"
   end
 end
