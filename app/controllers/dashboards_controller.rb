474872b0fbd83aef748088e2de986b6a6808b9fa
Rolando Navarrete <kinglouis.belmont@gmail.com>
2015-01-15 15:00:54 -0700

    removed references to nil variables in the dashboards show view

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,7 +2,5 @@ class DashboardsController < ApplicationController
   before_filter :require_login
 
   def show
-    @applications = current_person.applications.active
-    @dead_applications = current_person.applications.dead
   end
 end
