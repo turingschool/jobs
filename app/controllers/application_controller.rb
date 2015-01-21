ec6f42e613ddb8fcb668b20e2ee08888903275ba
awortham <wortham.aaron@gmail.com>
2015-01-21 15:59:02 -0700

    testing teardown

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :logged_in?
 
   def logged_in?
     !!session[:user_id]
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,8 +5,10 @@ require "capybara/rails"
 require "minitest/emoji"
 require "rack_session_access/capybara"
 require "database_cleaner"
+DatabaseCleaner.strategy = :truncation
 
 class ActiveSupport::TestCase
+
   def setup
     DatabaseCleaner.start
   end
@@ -16,8 +18,12 @@ class ActiveSupport::TestCase
   end
 
   include FactoryGirl::Syntax::Methods
+
 end
 
 class ActionDispatch::IntegrationTest
   include Capybara::DSL
+  def teardown
+    page.set_rack_session(user_id: nil)
+  end
 end
