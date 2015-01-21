67af1e42c16795f44ee6670bac98a6c9af55cf59
awortham <wortham.aaron@gmail.com>
2015-01-20 19:59:37 -0700

    hounding

--- test/features/filters_spec.rb
+++ test/features/filters_spec.rb
@@ -3,11 +3,11 @@ require "./test/test_helper"
 class Filters < ActionDispatch::IntegrationTest
   def test_it_filters_by_status_in_columns
     user = create(:person)
-    create(:application, person_id: user.id )
+    create(:application, person_id: user.id)
     create(:application, status: "applied",
                          company: "quickleft",
-                         person_id: user.id )
-                         
+                         person_id: user.id)
+
     page.set_rack_session(user_id: user.id)
     visit dashboard_path
 
