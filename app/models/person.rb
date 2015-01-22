3b49971ea329bae1280754eddf4dc9132dc92e6d
awortham <wortham.aaron@gmail.com>
2015-01-22 16:07:15 -0700

    cleaned up some code

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,4 @@
 class Person < ActiveRecord::Base
-  # validates_uniqueness_of :user_id
   has_many :applications
 
   def self.editable_attributes
@@ -7,14 +6,12 @@ class Person < ActiveRecord::Base
   end
 
   def self.find_or_create_user_from(auth)
-    Rails.logger.info("got omniauth auth data: #{auth.inspect}")
     person = find_or_create_by(provider: auth.provider, uid: auth.uid)
 
     person.provider     = auth.provider
     person.uid          = auth.uid
     person.first_name   = auth.info.name
     person.oauth_token  = auth.credentials.token
-    Rails.logger.info("making or updating a person; valid? #{person.valid?}, errors: #{person.errors}")
     person.save!
 
     person
