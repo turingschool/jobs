a10c86da9fc128b7497f402df67a4edbd7f01f39
Sara Simon <smbsimon@gmail.com>
2015-01-25 14:09:21 -0700

    Making progress on the drag and drop. Set up jQuery UI requirements, JavaScript functionality, and correct CSS. Now to debug

---
+++ app/assets/javascripts/applications.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+  $( ".column" ).sortable({
+    connectWith: ".column",
+    handle: ".portlet-header",
+    cancel: ".portlet-toggle",
+    placeholder: "portlet-placeholder ui-corner-all",
+    receive: function (event, ui) { alert("You dragged me into \"" + $(this).find('h2').text() + "\""); }
+  });
+
+  $( ".portlet" )
+  .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
+  .find( ".portlet-header" )
+  .addClass( "ui-widget-header ui-corner-all" )
+  .prepend( "<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
+
+
+  $( ".portlet-toggle" ).click(function() {
+    var icon = $( this );
+    icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
+    icon.closest( ".portlet" ).find( ".portlet-content" ).toggle();
+  });
+
+});
